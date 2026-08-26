24d53538f02e24492d3a147615ae2192c1dedab3
bereket2356-bit <bereket2356@gmail.com>
2026-08-27 01:56:01 +0300

    feat: add AlertProvider to RootLayout for managing alerts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import { Stack } from "expo-router";
 import { ItemProvider } from "../context/Itemscontext";
 import { UserProvider } from "../context/Usercontext";
+import { AlertProvider } from "../context/AlertContext";
 
 export default function RootLayout() {
   return (
     <ItemProvider>
       <UserProvider>
+        <AlertProvider>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
         </Stack>
+            </AlertProvider>
       </UserProvider>
     </ItemProvider>
   );
