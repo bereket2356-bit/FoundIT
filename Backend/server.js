7cec8fb270e198709427f4d5352167775f9854af
bereket2356-bit <bereket2356@gmail.com>
2026-08-27 01:56:40 +0300

    feat: reorganize route declarations for clarity and consistency

--- Backend/server.js
+++ Backend/server.js
@@ -9,14 +9,15 @@ connectDB();
 const app = express();
 const claimRoutes = require("./routes/claim");
 
-app.use("/api/claims", claimRoutes);
-
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/claims", claimRoutes);
+
 // ROUTES
 app.use("/api/auth", require("./routes/auth"));
-app.use("/api/items", require("./routes/items")); // 👈 ADD THIS
+app.use("/api/items", require("./routes/items"));
+app.use("/api/admin", require("./routes/admin")); // 👈 ADD THIS
 
 app.get("/", (req, res) => {
   res.send("API Running...");
