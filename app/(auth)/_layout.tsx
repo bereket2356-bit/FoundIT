bd3c8a7882d6839b9eff0e2e9b200128b771bf16
bereket2356-bit <bereket2356@gmail.com>
2026-08-27 23:17:08 +0300

    feat: add verify screen to AuthLayout for enhanced user flow

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -6,6 +6,7 @@ export default function AuthLayout() {
       <Stack.Screen name="login" />
       <Stack.Screen name="signup" />
       <Stack.Screen name="forgotpassword" />
+      <Stack.Screen name="verify" />
     </Stack>
   );
 }
