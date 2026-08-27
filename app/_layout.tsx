67c8d4453fe8e220d96d0e7cdc7b48c31f7a1005
bereket2356-bit <bereket2356@gmail.com>
2026-08-27 23:16:56 +0300

    feat: restructure RootLayout for improved layout and styling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,27 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AlertProvider } from "../context/AlertContext";
 import { ItemProvider } from "../context/Itemscontext";
 import { UserProvider } from "../context/Usercontext";
-import { AlertProvider } from "../context/AlertContext";
 
 export default function RootLayout() {
   return (
-    <ItemProvider>
-      <UserProvider>
-        <AlertProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(tabs)" />
-        </Stack>
-            </AlertProvider>
-      </UserProvider>
-    </ItemProvider>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <ItemProvider>
+        <UserProvider>
+          <AlertProvider>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+                contentStyle: { backgroundColor: "#f8f9fa" },
+              }}
+            >
+              <Stack.Screen name="(auth)" />
+              <Stack.Screen name="(tabs)" />
+            </Stack>
+          </AlertProvider>
+        </UserProvider>
+      </ItemProvider>
+    </SafeAreaProvider>
   );
 }
