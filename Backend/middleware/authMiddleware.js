31780aa5d858eb8d9190c6436e0094dfb13a134f
bereket2356-bit <bereket2356@gmail.com>
2026-09-03 20:45:47 +0300

    fix: improve error handling in authMiddleware for user verification and token validation

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -13,9 +13,16 @@ const protect = async (req, res, next) => {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
+      if (!req.user) {
+        return res
+          .status(401)
+          .json({ message: "Not authorized, user not found" });
+      }
       next();
     } catch (error) {
-      res.status(401).json({ message: "Not authorized" });
+      res
+        .status(401)
+        .json({ message: "Not authorized, token invalid or expired" });
     }
   } else {
     res.status(401).json({ message: "No token" });
